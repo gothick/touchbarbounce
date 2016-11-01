a58e6b351d4927816b84ad4da8d5c858878a63a9
Matt Gibson <gothick@gothick.org.uk>
2016-11-01 23:09:14 +0000

    Some basic animations going okay.

--- TouchBarBounce/WindowController.swift
+++ TouchBarBounce/WindowController.swift
@@ -9,11 +9,38 @@
 import Cocoa
 
 class WindowController: NSWindowController {
+
+    @IBOutlet weak var cell0: NSView!
+    @IBOutlet weak var cell1: NSView!
+    @IBOutlet weak var cell2: NSView!
+    @IBOutlet weak var cell3: NSView!
+    @IBOutlet weak var cell4: NSView!
+    @IBOutlet weak var cell5: NSView!
+    @IBOutlet weak var cell6: NSView!
+    @IBOutlet weak var cell7: NSView!
+    
+    var cells: [NSView] { return [cell0, cell1, cell2, cell3, cell4, cell5, cell6, cell7] }
     override func windowDidLoad() {
-        super.windowDidLoad();
+        super.windowDidLoad()
+        print(cells)
+
+        let anim = CABasicAnimation(keyPath: "backgroundColor")
+        anim.fromValue = NSColor.black.cgColor
+        anim.toValue = NSColor.blue.cgColor
+        anim.fillMode = kCAFillModeForwards
+        anim.isRemovedOnCompletion = false
+
+        for (index, cell) in cells.enumerated() {
+            cell.wantsLayer = true
+            anim.duration = 1 * CFTimeInterval(index)
+            cell.layer?.add(anim, forKey: nil)
+        }
     }
 }
 
+
+
+/*
 @available(OSX 10.12.1, *)
 extension WindowController: NSTouchBarDelegate {
     override func makeTouchBar() -> NSTouchBar? {
@@ -51,3 +78,4 @@ extension NSTouchBarItemIdentifier {
     static let launch = NSTouchBarItemIdentifier("uk.co.gothick.bounce.launch")
     static let hyperspace = NSTouchBarItemIdentifier("uk.co.gothick.bounce.hyperspace")
 }
+*/
