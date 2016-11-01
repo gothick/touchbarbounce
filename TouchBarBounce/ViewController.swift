416f7ec3c7fc017a90b15aeff521784844f41308
Matt Gibson <gothick@gothick.org.uk>
2016-11-01 10:55:27 +0000

    Initial Commit

---
+++ TouchBarBounce/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TouchBarBounce
+//
+//  Created by Matt Gibson on 01/11/2016.
+//  Copyright © 2016 Matt Gibson. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
